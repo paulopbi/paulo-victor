9fd482f2933931ba2177451e914b9e0a10c06ab1
Paulo Victor <paulovictordev16@gmail.com>
2026-03-11 18:47:58 -0300

    feat: initial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,20 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
