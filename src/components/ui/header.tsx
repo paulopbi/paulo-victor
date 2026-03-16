25d5866f91d81f399e3ee03218fcb2900bbc2b24
Paulo Victor <paulovictordev16@gmail.com>
2026-03-15 21:29:04 -0300

    feat: add header component

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,49 @@
+export const Header = ({
+  children,
+  ...props
+}: React.ComponentProps<"header">) => {
+  return (
+    <header className="mx-auto max-w-90 md:max-w-100" {...props}>
+      {children}
+    </header>
+  );
+};
+
+export const HeaderTitle = ({
+  children,
+  ...props
+}: React.ComponentProps<"h2">) => {
+  return (
+    <h2
+      className="text-center text-2xl lg:text-3xl font-bold text-balance"
+      {...props}
+    >
+      {children}
+    </h2>
+  );
+};
+
+export const HeaderDescription = ({
+  children,
+  ...props
+}: React.ComponentProps<"p">) => {
+  return (
+    <p
+      className="text-center text-sm lg:text-base font-normal text-muted-foreground text-pretty mt-2"
+      {...props}
+    >
+      {children}
+    </p>
+  );
+};
+
+export const HeaderSeparator = ({ ...props }: React.ComponentProps<"div">) => {
+  return (
+    <div
+      className="mx-auto w-11 h-0.5 bg-foreground/10 mt-3 rounded-full"
+      {...props}
+    >
+      <span className="sr-only">Separator</span>
+    </div>
+  );
+};
