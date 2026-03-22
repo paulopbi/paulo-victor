1c63e7c8a29792215449b8e80e76458c20739531
Paulo Victor <paulovictordev16@gmail.com>
2026-03-21 23:16:13 -0300

    refactor: remove geist mono font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import Footer from "@/components/ui/footer";
 import Navbar from "@/components/ui/navbar";
-import { geistMono, geistSans } from "@/fonts";
+import { geistSans } from "@/fonts";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
