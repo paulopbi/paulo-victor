23782e2ee64518d293ca0e6fbfc5c855de4e96a3
Paulo Victor <paulovictordev16@gmail.com>
2026-03-22 21:37:36 -0300

    refactor: add motion 'variants' interface

--- src/constants/animations.ts
+++ src/constants/animations.ts
@@ -1,5 +1,7 @@
-export const entranceAnimation = {
-  hidden: { opacity: 0, y: -20, filter: "blur(10px)" },
+import type { Variants } from "motion";
+
+export const entranceAnimation: Variants = {
+  hidden: { opacity: 0, y: -10, filter: "blur(20px)" },
   show: { opacity: 1, y: "initial", filter: "blur(0px)" },
 };
 
