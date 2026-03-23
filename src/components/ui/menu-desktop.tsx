63a1ab7c288accd0fb5abec8bfa4c9112c8d3e03
Paulo Victor <paulovictordev16@gmail.com>
2026-03-22 21:36:56 -0300

    feat: add stagger menu items animation

--- src/components/ui/menu-desktop.tsx
+++ src/components/ui/menu-desktop.tsx
@@ -1,24 +1,35 @@
+import { motion } from "motion/react";
+import {
+  navbarContainerVariants,
+  navbarItemVariants,
+} from "@/constants/animations";
 import { navigationLinks } from "@/constants/navigation-links";
 import { AnimatedThemeToggler } from "./animated-theme-toggler";
 
 const MenuDesktop = () => {
   return (
     <nav className="hidden md:flex items-center justify-end gap-5">
-      <div className="flex items-center gap-4">
+      <motion.ul
+        className="flex items-center justify-end gap-4"
+        variants={navbarContainerVariants}
+        initial="hidden"
+        animate="visible"
+      >
         {navigationLinks.map(({ label, href }) => (
-          <a
-            key={label}
-            href={href}
-            className="inline-block font-medium tracking-normal text-base text-foreground/80 hover:text-primary hover:-translate-y-1 transition-all duration-200"
-          >
-            {label}
-          </a>
+          <motion.li variants={navbarItemVariants} key={label}>
+            <a
+              href={href}
+              className="inline-block font-medium tracking-normal text-base text-foreground/80 hover:text-primary hover:-translate-y-1 transition-all duration-200"
+            >
+              {label}
+            </a>
+          </motion.li>
         ))}
-      </div>
 
-      <div className="flex items-center justify-center gap-4">
-        <AnimatedThemeToggler className="toggle-theme-button" />
-      </div>
+        <li className="flex items-center justify-center gap-4">
+          <AnimatedThemeToggler className="toggle-theme-button" />
+        </li>
+      </motion.ul>
     </nav>
   );
 };
--- src/constants/animations.ts
+++ src/constants/animations.ts
@@ -2,3 +2,22 @@ export const entranceAnimation = {
   hidden: { opacity: 0, y: -20, filter: "blur(10px)" },
   show: { opacity: 1, y: "initial", filter: "blur(0px)" },
 };
+
+export const navbarContainerVariants: Variants = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+export const navbarItemVariants: Variants = {
+  hidden: { y: -20, opacity: 0, filter: "blur(24px)" },
+  visible: {
+    y: 0,
+    opacity: 1,
+    filter: "blur(0px)",
+    transition: { duration: 0.4 },
+  },
+};
