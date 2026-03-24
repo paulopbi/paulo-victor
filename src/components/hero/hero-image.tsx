2484266c907e5ffe0c17648a27c9875aa5bfe8b8
Paulo Victor <paulovictordev16@gmail.com>
2026-03-24 20:47:50 -0300

    feat: add animations

--- src/components/hero/hero-image.tsx
+++ src/components/hero/hero-image.tsx
@@ -1,16 +1,18 @@
+"use client";
+
 import { motion } from "motion/react";
 import Image from "next/image";
-import { entranceAnimation } from "@/constants/animations";
+import { slideTop } from "@/constants/animations";
 import HERO_IMG from "../../../public/img/hero-img.webp";
 
 const HeroImage = () => {
   return (
     <motion.div
-      variants={entranceAnimation}
+      variants={slideTop}
       initial="hidden"
       animate="show"
-      transition={{ ease: "easeIn" }}
-      className="size-80 rounded-full overflow-hidden border-2 border-foreground hover:-translate-y-4 hover:shadow-lg hover:scale-[1.1] transition-all duration-100"
+      transition={{ ease: "linear", duration: 0.5, delay: 0.2 }}
+      className="size-70 rounded-full overflow-hidden border-2 border-foreground hover:shadow-lg hover:-translate-y-4 transition-all"
     >
       <Image
         src={HERO_IMG}
