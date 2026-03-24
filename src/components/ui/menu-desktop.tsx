d2bf3eced4f2c579268b0b713d6ec7991cc33d23
Paulo Victor <paulovictordev16@gmail.com>
2026-03-24 20:43:32 -0300

    feat: add animations

--- src/components/ui/menu-desktop.tsx
+++ src/components/ui/menu-desktop.tsx
@@ -1,7 +1,8 @@
 import { motion } from "motion/react";
 import {
   navbarContainerVariants,
-  navbarItemVariants,
+  navbarLinksVariants,
+  slideTopWithBlur,
 } from "@/constants/animations";
 import { navigationLinks } from "@/constants/navigation-links";
 import { AnimatedThemeToggler } from "./animated-theme-toggler";
@@ -16,19 +17,25 @@ const MenuDesktop = () => {
         animate="visible"
       >
         {navigationLinks.map(({ label, href }) => (
-          <motion.li variants={navbarItemVariants} key={label}>
+          <motion.li variants={navbarLinksVariants} key={label}>
             <a
               href={href}
-              className="inline-block font-medium tracking-normal text-base text-foreground/80 hover:text-primary hover:-translate-y-1 transition-all duration-200"
+              className="inline-block font-medium tracking-normal text-base border-b border-transparent pb-0.5 text-foreground hover:border-b-primary hover:text-primary dark:hover:text-chart-1 hover:-translate-y-1 transition-all duration-200"
             >
               {label}
             </a>
           </motion.li>
         ))}
 
-        <li className="flex items-center justify-center gap-4">
-          <AnimatedThemeToggler className="toggle-theme-button" />
-        </li>
+        <motion.li
+          variants={slideTopWithBlur}
+          initial="hidden"
+          animate="show"
+          transition={{ duration: 1, ease: "easeInOut", delay: 2.9 }}
+          className="flex items-center justify-center gap-4 p-1 rounded-full border border-transparent hover:border-primary/80 hover:bg-primary/20 hover:text-primary dark:hover:border-chart-1/60 dark:hover:bg-primary dark:hover:text-chart-1 transition-all"
+        >
+          <AnimatedThemeToggler className="cursor-pointer" />
+        </motion.li>
       </motion.ul>
     </nav>
   );
