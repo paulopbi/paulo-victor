6b47ef262ff63b0d4a2d2764b96d1ff544f57973
Paulo Victor <paulovictordev16@gmail.com>
2026-03-29 10:25:04 -0300

    fix: z-index value

--- src/components/particle-container.tsx
+++ src/components/particle-container.tsx
@@ -12,7 +12,7 @@ const ParticleContainer = () => {
   };
   return (
     <Particles
-      className="absolute inset-0 -z-10 size-full"
+      className="absolute inset-0 z-0 size-full"
       quantity={130}
       size={0.8}
       color={resolvedTheme === "dark" ? colors.light : colors.dark}
