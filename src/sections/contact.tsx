a21171503404ec7788606c7a8e3347f817a39218
Paulo Victor <paulovictordev16@gmail.com>
2026-03-29 20:31:38 -0300

    feat: remove social links and add a title and button

--- src/sections/contact.tsx
+++ src/sections/contact.tsx
@@ -1,6 +1,7 @@
-import GrainietBackground from "@/components/contact/grainiet-background";
-import LinksContainer from "@/components/contact/links-container";
-
+import { ExternalLink } from "lucide-react";
+import { InteractiveHoverButton } from "@/components/magic-ui/interactive-hover-button";
+import Grainient from "@/components/react-bits/grainient";
+import { GRAINIET_PROPS } from "@/constants/props";
 import type { SectionProps } from "@/types";
 
 const ContactSection = ({ ...props }: SectionProps) => {
@@ -10,8 +11,19 @@ const ContactSection = ({ ...props }: SectionProps) => {
       {...props}
     >
       <div className="size-full relative">
-        <GrainietBackground />
-        <LinksContainer />
+        <Grainient
+          {...GRAINIET_PROPS}
+          className="size-full rounded-2xl absolute inset-0 z-0"
+        />
+
+        <div className="absolute left-1/2 top-1/2 -translate-1/2 flex flex-col gap-4 items-center justify-center flex-wrap w-full">
+          <h6 className="text-base md:text-lg font-medium">
+            Me siga nas redes sociais
+          </h6>
+          <InteractiveHoverButton icon={<ExternalLink className="icon-size" />}>
+            Todos os contatos
+          </InteractiveHoverButton>
+        </div>
       </div>
     </section>
   );
