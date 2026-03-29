6ac1f330b5e6de600cca038c4b6bcdf8f8e9bf73
Paulo Victor <paulovictordev16@gmail.com>
2026-03-29 11:44:34 -0300

    fix: change the import text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ParticleContainer from "@/components/particle-container";
 import ContactSection from "@/sections/contact";
 import ExperienceSection from "@/sections/experience";
 import HeroSection from "@/sections/hero";
-import ProjectsSection from "@/sections/projects";
+import ProjectsSection from "@/sections/project";
 import SkillsSection from "@/sections/skills";
 
 export default function HomePage() {
