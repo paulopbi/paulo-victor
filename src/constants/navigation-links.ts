fbc0e04bfa3b447d0d66f7523d2f9c779c7d1bf9
Paulo Victor <paulovictordev16@gmail.com>
2026-03-29 10:24:44 -0300

    fix: href string text

--- src/constants/navigation-links.ts
+++ src/constants/navigation-links.ts
@@ -1,7 +1,7 @@
 export const navigationLinks = [
   { label: "Sobre", href: "#sobre" },
   { label: "Habilidades", href: "#habilidades" },
-  { label: "Experiências", href: "#experiencia" },
+  { label: "Experiências", href: "#experiencias" },
   { label: "Projetos", href: "#projetos" },
   { label: "Contatos", href: "#contatos" },
 ];
