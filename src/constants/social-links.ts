c39ee9822772ec7b23f1c19f42201fd370b20ed2
Paulo Victor <paulovictordev16@gmail.com>
2026-03-29 20:32:55 -0300

    refactor: remove Twitter from social links

--- src/constants/social-links.ts
+++ src/constants/social-links.ts
@@ -1,4 +1,4 @@
-import { Github, Instagram, Linkedin, Mail, Twitter } from "lucide-react";
+import { Github, Instagram, Linkedin, Mail } from "lucide-react";
 
 export const socialLinks = [
   { icon: Github, label: "Github", href: "https://github.com/paulopbi" },
@@ -12,6 +12,5 @@ export const socialLinks = [
     label: "Instagram",
     href: "https://www.instagram.com/xyz.paulovictor",
   },
-  { icon: Twitter, label: "Twitter", href: "https://github.com/paulopbi" },
   { icon: Mail, label: "E-Mail", href: "mailto:paulovictordev16@gmail.com" },
 ];
