9ea4f4d3907eb13c766e31cfd9e6b0ab8ba6a816
Paulo Victor <paulovictordev16@gmail.com>
2026-03-30 21:54:50 -0300

    refactor: update ProjectCTA to use external link constants

--- src/components/project/project-cta.tsx
+++ src/components/project/project-cta.tsx
@@ -1,11 +1,12 @@
 import { Github } from "lucide-react";
-import { InteractiveHoverButton } from "../magic-ui/interactive-hover-button";
+import { InteractiveHoverButton } from "@/components/magic-ui/interactive-hover-button";
+import { GH_ALL_PROJECTS } from "@/constants/external-links";
 
 const ProjectCTA = () => {
   return (
     <div className="grid place-items-center mt-14">
       <a
-        href="https://github.com/paulopbi?tab=repositories"
+        href={GH_ALL_PROJECTS}
         rel="noreferrer"
         target="_blank"
         className="size-fit flex items-center justify-center gap-2"
---
+++ src/constants/external-links.ts
@@ -0,0 +1,4 @@
+export const CV_URL =
+  "https://docs.google.com/document/d/1MFXpPABaiJhJDr8cfxME1JEwZ3XKHS3Dx0UDAJL_T50/edit?usp=sharing";
+
+export const GH_ALL_PROJECTS = "https://github.com/paulopbi?tab=repositories";
