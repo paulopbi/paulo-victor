b30abaeb21f0f7508bcbe9fa07aee4b064cb702c
Paulo Victor <paulovictordev16@gmail.com>
2026-03-30 22:36:45 -0300

    feat: update external links constants for consistency

--- src/constants/external-links.ts
+++ src/constants/external-links.ts
@@ -1,4 +1,6 @@
 export const CV_URL =
   "https://docs.google.com/document/d/1MFXpPABaiJhJDr8cfxME1JEwZ3XKHS3Dx0UDAJL_T50/edit?usp=sharing";
 
-export const GH_ALL_PROJECTS = "https://github.com/paulopbi?tab=repositories";
+export const ALL_PROJECTS = "https://github.com/paulopbi?tab=repositories";
+
+export const ALL_MY_LINKS = "https://github.com/paulopbi?tab=repositories";
