a5a5926f891e5b2dc24d1c65bba1376a76363929
Paulo Victor <paulovictordev16@gmail.com>
2026-03-30 22:09:08 -0300

    feat: implement mobile menu state management and toggle functionality

--- src/components/navbar/menu-mobile.tsx
+++ src/components/navbar/menu-mobile.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Menu } from "lucide-react";
+import { useState } from "react";
 import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 import {
   Sheet,
@@ -10,11 +13,17 @@ import {
 import { internalLinks } from "@/constants/internal-links";
 
 const MenuMobile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = (openState: boolean) => {
+    setIsOpen(openState);
+  };
+
   return (
     <nav className="md:hidden flex items-center justify-end gap-6">
       <AnimatedThemeToggler className="toggle-theme-button" />
 
-      <Sheet>
+      <Sheet open={isOpen} onOpenChange={handleClose}>
         <SheetTrigger>
           <Menu className="icon-size cursor-pointer hover:text-primary transition-colors" />
           <span className="sr-only">Mobile Menu</span>
@@ -30,6 +39,7 @@ const MenuMobile = () => {
             <div className="flex flex-col pl-4 gap-6">
               {internalLinks.map(({ href, label }) => (
                 <a
+                  onClick={() => setIsOpen(false)}
                   key={label}
                   href={href}
                   className="inline-block font-medium tracking-normal pb-1 text-base md:text-lg hover:text-primary dark:hover:text-chart-1 hover:-translate-y-1 transition-all duration-200"
