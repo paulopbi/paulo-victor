ab13b46b97ee24781510e0ecf0e0d44263a6ef5e
Paulo Victor <paulovictordev16@gmail.com>
2026-04-02 23:35:52 -0300

    chore: remove unused images configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [{ hostname: "images.unsplash.com" }],
-  },
 };
 
 export default nextConfig;
