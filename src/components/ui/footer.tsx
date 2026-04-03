0cbf05a725b6815d9eb2fe0f917ee400be403f7e
Paulo Victor <paulovictordev16@gmail.com>
2026-04-02 23:33:23 -0300

    refactor: update footer component layout and dynamic year display

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,28 +1,25 @@
 import { SOCIAL_MEDIA_LINKS } from "@/constants/external-links";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer className="main-container">
-      <div className="flex flex-wrap items-center justify-center md:justify-between gap-4 py-6 px-4">
-        <div>
-          <p className="text-muted-foreground text-center text-sm lg:text-base">
-            &copy; 2026 - Paulo Victor, todos os direitos reservados.
-          </p>
-        </div>
+    <footer className="main-container flex flex-col flex-wrap gap-4 items-center justify-center py-2 mt-8 md:flex-row md:justify-between">
+      <p className="text-muted-foreground text-center text-base md:text-left">
+        &copy; {currentYear} todos os direitos reservados.
+      </p>
 
-        <div className="flex items-center justify-end gap-4">
-          {SOCIAL_MEDIA_LINKS.map(({ icon: Icon, label, href }) => (
-            <a
-              href={href}
-              key={label}
-              target="_blank"
-              className="p-2 rounded-full flex items-center justify-center gap-2 transition-all hover:bg-primary/10 hover:text-primary hover:border-primary/80 hover:-translate-y-1"
-            >
-              {<Icon />}
-              <span className="sr-only">{label}</span>
-            </a>
-          ))}
-        </div>
+      <div className="flex items-center justify-end gap-4">
+        {SOCIAL_MEDIA_LINKS.map(({ icon: Icon, label, href }) => (
+          <a
+            href={href}
+            key={label}
+            target="_blank"
+            className="p-2 rounded-full flex items-center justify-center gap-2 text-hover transition-all hover:-translate-y-1"
+          >
+            {<Icon className="size-6" />}
+            <span className="sr-only">{label}</span>
+          </a>
+        ))}
       </div>
     </footer>
   );
