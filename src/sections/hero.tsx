37e981e31716629ffbab7925602e2e1cf0d194e7
Paulo Victor <paulovictordev16@gmail.com>
2026-04-06 18:45:41 -0300

    feat: add silk animation and change the HeroBadge content

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { motion } from "motion/react";
 import CTA from "@/components/call-to-action";
 import InfinityArrow from "@/components/hero/animated-arrow-down";
 import {
@@ -10,23 +9,21 @@ import {
   HeroImage,
   HeroTitle,
 } from "@/components/hero/hero-content";
-import LiquidEther from "@/components/react-bits/liquid-ether";
+import Silk from "@/components/react-bits/silk";
 import { slideRightWithBlur } from "@/constants/animations";
 import { CV_URL } from "@/constants/external-links";
-import { LIQUID_ETHER_PROPS } from "@/constants/props";
+import { SILK_PROPS } from "@/constants/props";
 import type { SectionProps } from "@/types";
+import { motion } from "motion/react";
 import HERO_IMG from "../../public/img/me-optimized.webp";
 
 const HeroSection = ({ ...props }: SectionProps) => {
   return (
     <section {...props} className="relative h-dvh overflow-hidden">
-      <LiquidEther
-        {...LIQUID_ETHER_PROPS}
-        className="size-full z-0 inset-0 absolute"
-      />
+      <Silk {...SILK_PROPS} />
 
       <HeroContainer>
-        <HeroBadge>Disponível Para Trabalho</HeroBadge>
+        <HeroBadge>Disponível para trabalho</HeroBadge>
         <HeroImage src={HERO_IMG} alt="Imagem do programador Paulo Victor" />
         <HeroTitle>Desenvolvedor Fullstack</HeroTitle>
         <HeroDescription>
